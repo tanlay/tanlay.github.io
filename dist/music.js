1077f7fbc0577dd609cd563ba6bc632b5e0afd93
tanlay <18313178130@163.com>
2021-06-12 22:41:16 +0800

    Site updated: 2021-06-12 22:41:15

--- dist/music.js
+++ dist/music.js
@@ -1,13 +1,21 @@
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
-    fixed: true,
-    autoplay: false,
+    autoplay: false, //自动播放
+	mini: false, //迷你模式
+    theme: '#FADFA3', //主题色
+    loop: 'all', //音频循环播放, 可选值: 'all'全部循环, 'one'单曲循环, 'none'不循环
+    order: 'random', //音频循环顺序, 可选值: 'list'列表循环, 'random'随机循环
+    preload: 'auto', //预加载，可选值: 'none', 'metadata', 'auto'
+    volume: 0.7, //默认音量，请注意播放器会记忆用户设置，用户手动设置音量后默认音量即失效
+    mutex: true, //互斥，阻止多个播放器同时播放，当前播放器播放时暂停其他播放器
+    listFolded: true, //列表默认折叠
+    listMaxHeight: 90, //列表最大高度
     audio: [
 	  {
         name: '勇气',
         artist: '棉子',
         url: 'http://music.163.com/song/media/outer/url?id=1411358329.mp3',
-        cover: '/images/5.jpg',
+        cover: '#',
       },
       {
         name: "星星",
