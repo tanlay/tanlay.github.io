16dd25dc4eaf58089f799a7a5736231af70e0580
tanlay <18313178130@163.com>
2021-06-13 09:34:57 +0800

    Site updated: 2021-06-13 09:34:57

--- dist/music.js
+++ dist/music.js
@@ -11,53 +11,105 @@ const ap = new APlayer({
     listFolded: true, //列表默认折叠
     listMaxHeight: 90, //列表最大高度
     audio: [
-	  {
-        name: '勇气',
-        artist: '棉子',
-        url: 'http://music.163.com/song/media/outer/url?id=1411358329.mp3',
-        cover: '#',
+      {
+        name: '天龙八部之宿敌',
+        artist: '许嵩',
+        url: 'http://music.163.com/song/media/outer/url?id=167691.mp3',
       },
       {
-        name: "星星",
+        name: '光年之外',
+        artist: '邓紫棋',
+        url: 'http://music.163.com/song/media/outer/url?id=486194122.mp3',
+      },
+      {
+        name: '挪威的森林',
+        artist: '伍佰',
+        url: 'http://music.163.com/song/media/outer/url?id=157288.mp3',
+      },
+      {
+        name: '等一分钟',
+        artist: '徐誉滕',
+        url: 'http://music.163.com/song/media/outer/url?id=1493994302.mp3',
+      },
+      {
+        name: '我们都一样',
         artist: '张杰',
-        url: 'http://music.163.com/song/media/outer/url?id=1399004693.mp3',
-        cover: '/images/xingxing.jpg',
+        url: 'http://music.163.com/song/media/outer/url?id=191252.mp3',
       },
       {
-        name: '这就是爱吗',
-        artist: '十豆',
-        url: 'http://music.163.com/song/media/outer/url?id=1412242872.mp3',
-        cover: '/images/zjsam.jpg',
+        name: '追梦人',
+        artist: '凤飞飞',
+        url: 'http://music.163.com/song/media/outer/url?id=231412.mp3',
       },
       {
-        name: 'Alone',
-        artist: 'Alan Walker / Noonie Bao',
-        url: 'http://music.163.com/song/media/outer/url?id=444269135.mp3',
-        cover: '/images/alone.jpg',
+        name: '最美的期待',
+        artist: '周笔畅',
+        url: 'http://music.163.com/song/media/outer/url?id=531295576.mp3',
+      },
+      {
+        name: '麻雀',
+        artist: '李荣浩',
+        url: 'http://music.163.com/song/media/outer/url?id=1407551413.mp3',
+      },
+      {
+        name: '大风吹',
+        artist: '王赫野',
+        url: 'http://music.163.com/song/media/outer/url?id=1826179038.mp3',
+      },
+      {
+        name: '囚鸟',
+        artist: '彭玲',
+        url: 'http://music.163.com/song/media/outer/url?id=5252280.mp3',
+      },
+      {
+        name: '往后余生',
+        artist: '马良',
+        url: 'http://music.163.com/song/media/outer/url?id=557584888.mp3',
+      },
+      {
+        name: '体面',
+        artist: '于文文',
+        url: 'http://music.163.com/song/media/outer/url?id=1388608541.mp3',
+      },
+      {
+        name: 'Cry On My Shoulder',
+        artist: 'Deutschland sucht den Superstar',
+        url: 'http://music.163.com/song/media/outer/url?id=17194024.mp3',
+      },
+      {
+        name: '遇萤（橙光游戏《遇龙》主题曲）',
+        artist: 'CRITTY',
+        url: 'http://music.163.com/song/media/outer/url?id=37782740.mp3',
+      },
+      {
+        name: '我的一个道姑朋友',
+        artist: '双笙',
+        url: 'http://music.163.com/song/media/outer/url?id=1367452194.mp3',
+      },
+      {
+        name: '年轮',
+        artist: '汪苏泷',
+        url: 'http://music.163.com/song/media/outer/url?id=32897777.mp3',
       },
 	  {
-        name: 'Umbrella (Matte Remix)',
-        artist: 'Matte / Ember Island',
-        url: 'http://music.163.com/song/media/outer/url?id=164209623.mp3',
-        cover: '/images/1.jpg',
+        name: '大鱼 (Live)',
+        artist: '周深',
+        url: 'http://music.163.com/song/media/outer/url?id=1421191783.mp3',
       },
 	  {
-        name: '大眠 (完整版)原唱：王心凌',
-        artist: '小乐哥',
-        url: 'http://music.163.com/song/media/outer/url?id=3778678.mp3',
-        cover: '/images/2.jpg',
+        name: '左手指月',
+        artist: '萨顶顶',
+        url: 'http://music.163.com/song/media/outer/url?id=536096151.mp3',
       },
 	  {
-        name: '世间美好与你环环相扣',
-        artist: '柏松',
-        url: 'http://music.163.com/song/media/outer/url?id=1363948882.mp3',
-        cover: '/images/3.jpg',
-      },
+        name: '好久不见',
+        artist: '陈奕迅',
+        url: 'http://music.163.com/song/media/outer/url?id=65538.mp3',
+      }
 	  {
-        name: '飞',
-        artist: '王恩信Est / 二胖u',
-        url: 'http://music.163.com/song/media/outer/url?id=1386259535.mp3',
-        cover: '/images/4.jpg',
+        name: '海角七号',
+        artist: '东来东往',
+        url: 'http://music.163.com/song/media/outer/url?id=1479528069.mp3',
       }
     ]
 });
